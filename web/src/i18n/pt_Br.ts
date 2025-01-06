df2b7059f5eb3a110ae69200cf71f565c154665a
Cephas369 <pedro-leitao@outlook.com>
2025-01-06 13:13:59 -0400

    fix: lint fix

--- web/src/i18n/pt_Br.ts
+++ web/src/i18n/pt_Br.ts
@@ -1,18 +1,18 @@
-import { defineLocale } from "aeria-ui";
+import { defineLocale } from 'aeria-ui'
 
 export const ptbr = defineLocale({
-    equipment_Release: "Liberação de Equipamento",
-    assets: "Equipamentos",
-    employee: "Funcionário",
-    corporate_email: "E-mail Corporativo",
-    is_active: "Ativo",
-    admission_date: "Data de Admissão",
-    exit_date: "Data de Saída",
-    name: "Nome",
-    code: "Código",
-    includes_accessories: "Inclui Acessórios",
-    registered_by: "Registrado por",
-    observation: "Observação",
-    delivered_to: "Entregue para",
-    delivered_by: "Entregue por",
-});
+    equipment_Release: 'Liberação de Equipamento',
+    assets: 'Equipamentos',
+    employee: 'Funcionário',
+    corporate_email: 'E-mail Corporativo',
+    is_active: 'Ativo',
+    admission_date: 'Data de Admissão',
+    exit_date: 'Data de Saída',
+    name: 'Nome',
+    code: 'Código',
+    includes_accessories: 'Inclui Acessórios',
+    registered_by: 'Registrado por',
+    observation: 'Observação',
+    delivered_to: 'Entregue para',
+    delivered_by: 'Entregue por',
+})
